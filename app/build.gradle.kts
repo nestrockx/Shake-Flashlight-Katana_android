8fa3b5f2397099dc20358a30e867c47addcef95c
Paweł <wegielek.art@gmail.com>
2026-03-10 12:29:44 +0100

    Update version to 2.0.7 and add Advertising ID permission
    - Increment `versionCode` to 20 and `versionName` to "2.0.7" in the build configuration and string resources.
    - Add the `com.google.android.gms.permission.AD_ID` permission to the manifest.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.wegielek.katana_flashlight"
         minSdk = 24
         targetSdk = 36
-        versionCode = 19
-        versionName = "2.0.6"
+        versionCode = 20
+        versionName = "2.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
